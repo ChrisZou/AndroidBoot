3ea33a8ec1c83235b17a9ec92ae91391d3267da6
xiaochuang <xiaochuang@mogujie.com>
2016-10-26 12:28:52 +0800

    Add demo for butterknife

--- app/src/main/java/com/mogujie/myapplication/MainActivity.java
+++ app/src/main/java/com/mogujie/myapplication/MainActivity.java
@@ -1,13 +1,23 @@
 package com.mogujie.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.msgView)
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        ButterKnife.bind(this);
+        mTextView.setText("yes, ButterKnife works");
     }
 }
